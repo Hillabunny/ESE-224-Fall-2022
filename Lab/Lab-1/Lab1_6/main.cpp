ec5bd37639edf23fdb40ebf7ba2a6b1d9bc91337
Hillabunny <98918650+Hillabunny@users.noreply.github.com>
2022-09-23 19:26:08 -0400

    Create main.cpp

---
+++ Lab/Lab-1/Lab1_6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Circle.h"
+
+using namespace std;
+
+int main() 
+{
+	double x, y, r;
+
+
+	cout << "Enter the center coordinate of the circle (x,y) and the radius: " << endl;
+	cin >> x >> y >> r;
+	
+
+	system("pause");
+
+	return 0;
+
+}
